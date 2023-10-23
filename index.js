804db452603d74472d2a36c51e63e3085374da49
Sch <junhg0211@gmail.com>
2023-10-23 13:37:07 +0900

    add date and timezone info

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let clockHour, clockMinute, clockSecond, clockMillisecond;
+let clockDate, clockTimezone;
 let container;
 let nowFullscreen = false;
 
@@ -8,6 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     clockMinute = document.querySelector("#clock-minute");
     clockSecond = document.querySelector("#clock-second");
     clockMillisecond = document.querySelector("#clock-millisecond");
+    clockDate = document.querySelector("#clock-date");
+    clockTimezone = document.querySelector("#clock-timezone");
 
     setInterval(() => {
         let time = new Date();
@@ -16,11 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
         let minutes = time.getMinutes();
         let seconds = time.getSeconds();
         let milliseconds = Math.floor(time.getMilliseconds() / 1000 * 60);
-
         clockHour.innerText = hours;
         clockMinute.innerText = padNumber(minutes, 2);
         clockSecond.innerText = padNumber(seconds, 2);
         clockMillisecond.innerText = padNumber(milliseconds, 2);
+
+        let year = time.getFullYear();
+        let month = time.getMonth();
+        let day = time.getDate();
+        clockDate.innerText = `${orderString(day)} ${monthString(month)}, ${year}`
+
+        var zone = time.toLocaleTimeString('en-us',{timeZoneName:'long'}).split(/ [AP]M /)[1];
+        clockTimezone.innerText = zone;
     }, 1000 / 60);
 
     // -- fullscreen
--- util.js
+++ util.js
@@ -15,3 +15,62 @@ function fullscreen() {
         rfs = el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
     rfs.call(el);
 }
+
+function orderString(number) {
+    if (number == 11 || number == 12 || number == 13) {
+        return `${number}th`;
+    }
+
+    if (number % 10 == 1) {
+        return `${number}st`;
+    }
+    if (number % 10 == 2) {
+        return `${number}nd`;
+    }
+    if (number % 10 == 3) {
+        return `${number}rd`;
+    }
+
+    return `${number}th`;
+}
+
+function monthString(month) {
+    month += 1;
+
+    if (month == 1) {
+        return "January";
+    }
+    if (month == 2) {
+        return "February";
+    }
+    if (month == 3) {
+        return "March";
+    }
+    if (month == 4) {
+        return "April";
+    }
+    if (month == 5) {
+        return "May";
+    }
+    if (month == 6) {
+        return "June";
+    }
+    if (month == 7) {
+        return "July";
+    }
+    if (month == 8) {
+        return "August";
+    }
+    if (month == 9) {
+        return "September";
+    }
+    if (month == 10) {
+        return "October";
+    }
+    if (month == 11) {
+        return "November";
+    }
+    if (month == 12) {
+        return "December";
+    }
+}
