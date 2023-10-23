bf9b0d1c1a899f73b50304763728fd64d0869567
Sch <junhg0211@gmail.com>
2023-10-23 16:19:59 +0900

    reduce updates

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let clockDate, clockTimezone;
 let container;
 let nowFullscreen = false;
 
+let previousHours, previousMinutes, previousSeconds;
+let previousYear, previousMonth, previousDay, previousWeekday;
+
 document.addEventListener('DOMContentLoaded', () => {
     // -- clock
     clockHour = document.querySelector("#clock-hour");
@@ -12,6 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     clockDate = document.querySelector("#clock-date");
     clockTimezone = document.querySelector("#clock-timezone");
 
+    let zone = new Date().toLocaleTimeString('en-us',{timeZoneName:'long'}).split(/ [AP]M /)[1];
+    clockTimezone.innerText = zone;
+
     setInterval(() => {
         let time = new Date();
 
@@ -20,9 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
         let minutes = time.getMinutes();
         let seconds = time.getSeconds();
         let milliseconds = Math.floor(time.getMilliseconds() / 1000 * 60);
-        clockHour.innerText = hours;
-        clockMinute.innerText = padNumber(minutes, 2);
-        clockSecond.innerText = padNumber(seconds, 2);
+        if (previousHours != hours) {
+            clockHour.innerText = hours;
+        }
+        if (previousMinutes != minutes) {
+            clockMinute.innerText = padNumber(minutes, 2);
+        }
+        if (previousSeconds != seconds) {
+            clockSecond.innerText = padNumber(seconds, 2);
+        }
         clockMillisecond.innerText = padNumber(milliseconds, 2);
 
         // -- date and timezone
@@ -30,16 +42,28 @@ document.addEventListener('DOMContentLoaded', () => {
         let month = time.getMonth();
         let day = time.getDate();
         let weekday = weekdayString(time.getDay());
-        let zone = time.toLocaleTimeString('en-us',{timeZoneName:'long'}).split(/ [AP]M /)[1];
-        clockDate.innerText = `${weekday}, ${padNumber(year, 4)}-${padNumber(month, 2)}-${padNumber(day, 2)}`;
-        clockTimezone.innerText = zone;
+        if (previousDay != day) {
+            clockDate.innerText = `${weekday}, ${padNumber(year, 4)}-${padNumber(month, 2)}-${padNumber(day, 2)}`;
+        }
 
         // -- theme
-        if (hours < 6 || hours >= 18) {
-            document.body.classList = ["dark"];
-        } else {
-            document.body.classList = ["light"];
+        if (previousHours != hours) {
+            if (hours < 6 || hours >= 18) {
+                document.body.classList.remove("light");
+                document.body.classList.add("dark");
+            } else {
+                document.body.classList.remove("dark");
+                document.body.classList.add("light");
+            }
         }
+
+        previousHours = hours;
+        previousMinutes = minutes;
+        previousSeconds = seconds;
+        previousYear = year;
+        previousMonth = month;
+        previousDay = day;
+        previousWeekday = weekday;
     }, 1000 / 60);
 
     // -- fullscreen
